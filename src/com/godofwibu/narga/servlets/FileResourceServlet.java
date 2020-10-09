c947bfdcc6ce9b494a8459ab4b440deb66dd2d6f
gintoki2000 <ngotrungaccor@gmail.com>
2020-10-09 10:50:44 +0700

    dùng bastract medthod thay cho dùng interface

--- src/com/godofwibu/narga/servlets/FileResourceServlet.java
+++ src/com/godofwibu/narga/servlets/FileResourceServlet.java
@@ -1,17 +1,66 @@
 package com.godofwibu.narga.servlets;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebInitParam;
 import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServletRequest;
 
 
-@WebServlet("/file/*")
+@WebServlet(urlPatterns = "/file/*", initParams = {
+		@WebInitParam(name = "prefix", value = "files")
+})
 public class FileResourceServlet extends ResourceServlet {
 	private static final long serialVersionUID = 1L;
-	private FileResourceResolver resolver;
+	private File directory;
+	
 	@Override
-	protected IResourceResolver getResourceResolver() {
-		if (resolver == null) {
-			resolver = new FileResourceResolver();
+	public void init(ServletConfig config) throws ServletException {
+		super.init(config);
+		
+		String prefix = "files";
+		
+		if (config.getInitParameter("prefix") != null) {
+			prefix = config.getInitParameter(prefix);
 		}
-		return resolver;
+		
+		directory = new File(prefix);
 	}
+	
+	@Override
+	protected IResource resolveResource(HttpServletRequest req) {
+		String name = req.getPathInfo();
+
+		if (name == null || name.isEmpty() || "/".equals(name)) {
+			throw new IllegalArgumentException();
+		}
+		
+		name = name.substring(1);
+		
+		File file = new File(directory, name);
+		
+		return new IResource() {
+			
+			@Override
+			public String getName() {
+				return file.getName();
+			}
+			
+			@Override
+			public InputStream getInputStream() throws IOException {
+				return new FileInputStream(file);
+			}
+			
+			@Override
+			public long getContentLength() {
+				return file.length();
+			}
+		};
+	}
+	
 }
--- src/com/godofwibu/narga/servlets/StaticResourceServlet.java
+++ src/com/godofwibu/narga/servlets/StaticResourceServlet.java
@@ -1,19 +1,67 @@
 package com.godofwibu.narga.servlets;
 
-import javax.servlet.annotation.WebServlet;
+import java.io.IOException;
+import java.io.InputStream;
 
-@WebServlet("/static/*")
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebInitParam;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServletRequest;
+
+@WebServlet(urlPatterns = "/static/*", initParams = {
+		@WebInitParam(name = "prefix", value = "/WEB-INF")
+})
 public class StaticResourceServlet extends ResourceServlet {
 	private static final long serialVersionUID = 1L;
-	
-	StaticResourceResolver resolver;
 
+	private String prefix;
+	
 	@Override
-	protected IResourceResolver getResourceResolver() {
-		if (resolver == null) {
-			resolver = new StaticResourceResolver(getServletContext());
-			resolver.setPrefix("/WEB-INF");
+	public void init(ServletConfig config) throws ServletException {
+		super.init(config);
+		
+		prefix = "/WEB-INF";
+		if (config.getInitParameter("prefix") != null) {
+			prefix = config.getInitParameter("prefix");
 		}
-		return resolver;
+	}
+	
+	@Override
+	protected IResource resolveResource(HttpServletRequest req) {
+		String name = req.getPathInfo();
+
+		if (name == null || name.isEmpty() || "/".equals(name)) {
+			throw new IllegalArgumentException();
+		}
+		
+		InputStream inputStream = getServletContext().getResourceAsStream(prefix + name);
+		
+		if (inputStream == null)
+			return null;
+		
+		String tmp = name;
+		return new IResource() {
+			
+			@Override
+			public String getName() {
+				return tmp;
+			}
+			
+			@Override
+			public InputStream getInputStream() throws IOException {
+				return inputStream;
+			}
+			
+			@Override
+			public long getContentLength() {
+				try {
+					return inputStream.available();
+				} catch (IOException e) {
+					e.printStackTrace();
+					return -1;
+				}
+			}
+		};
 	}
 }

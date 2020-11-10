3451a09aebb7c5b4cf956e451c52f5e926f2f059
Long-dl <59820153+Long-dl@users.noreply.github.com>
2020-11-10 18:17:48 +0700

    them back end sơ actor detail

---
+++ src/com/godofwibu/narga/dto/ActorDetail.java
@@ -0,0 +1,42 @@
+package com.godofwibu.narga.dto;
+
+
+import java.util.HashSet;
+import java.util.Set;
+
+import com.godofwibu.narga.entities.Actor;
+import com.godofwibu.narga.entities.Country;
+import com.godofwibu.narga.entities.Film;
+import com.godofwibu.narga.entities.Gender;
+
+import lombok.Data;
+
+
+@Data
+public class ActorDetail {
+	private String name;
+	private String gender;
+	private Integer age;
+	private String avatar;
+	private Country country;
+	private Set<FilmMinimumDetail> films;
+	public ActorDetail(Actor actor) {
+		this.name = actor.getName();
+		this.gender = actor.getGender() == Gender.MALE ? "Nam":"Nữ";
+		this.age = actor.getAge();
+		this.avatar = actor.getAvatar().getUrl();
+		this.country = actor.getCountry();
+		this.films = new HashSet<FilmMinimumDetail>();
+		for(Film film : actor.getFilms() ) {
+			films.add(new FilmMinimumDetail(film));
+		}
+	}
+
+
+	
+	
+	
+	
+	
+
+}
